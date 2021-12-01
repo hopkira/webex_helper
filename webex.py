8fb746b48f5d39e421e7b97f660808e0fabc4ac4
Richard Hopkins <hopkira@googlemail.com>
2021-12-01 22:16:18 +0000

    Process detection

--- webex.py
+++ webex.py
@@ -1,15 +1,38 @@
 import requests
 import time
+import psutil
 
-base_url = 'http://octopi.local:8123/api/webhook/'
-myobj = {'dummykey': 'dummyvalue'}
+def checkforprocess(processName):
+    '''
+    Check if there is any running process that contains the given name processName.
+    '''
+    #Iterate over the all the running process
+    for proc in psutil.process_iter():
+        try:
+            # Check if process name contains the given name string.
+            if processName.lower() in proc.exe().lower():             
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False
 
-on = 'webex_start'
-off = 'webex_finish'
+def checkforwebex():
+    result = checkforprocess("Meeting Center.app/Contents/Helpers/webexmta.app/Contents/MacOS/webexmta")
+    return result
 
-url_on = base_url + on
-url_off = base_url + off
+def webex(status):
+    '''
+    Execute web service to start flow
+    '''
+    service = "webex_finish"
+    base_url = 'http://octopi.local:1880/'
+    if status: service = "webex_start"
+    myobj = {'status': service}
+    url_on = base_url + service
+    requests.post(url_on, data = myobj)
 
-requests.post(url_on, data = myobj)
-time.sleep(1)
-requests.post(url_off, data = myobj)
+while True:
+    result = checkforwebex()
+    webex(result)
+    print(result)
+    time.sleep(1)
